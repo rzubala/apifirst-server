1ec16b3b8e735405e58516720aa1d664aa6db71c
Rafał Zubala <rzubala@gmail.com>
2024-05-25 15:32:38 +0200

    get customer

--- apifirst-server/src/main/java/guru/springframework/apifirst/apifirstserver/controllers/CustomerController.java
+++ apifirst-server/src/main/java/guru/springframework/apifirst/apifirstserver/controllers/CustomerController.java
@@ -5,10 +5,12 @@ import guru.springframework.apifirst.model.Customer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 import static guru.springframework.apifirst.apifirstserver.controllers.CustomerController.BASE_URL;
 
@@ -24,5 +26,10 @@ public class CustomerController {
     @GetMapping
     public ResponseEntity<List<Customer>> listCustomers() {
         return ResponseEntity.ok(customerService.listCustomers());
+    }
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable("customerId") UUID customerId) {
+        return ResponseEntity.ok(customerService.getCustomerById(customerId));
     }
 }
--- apifirst-server/src/main/java/guru/springframework/apifirst/apifirstserver/services/CustomerService.java
+++ apifirst-server/src/main/java/guru/springframework/apifirst/apifirstserver/services/CustomerService.java
@@ -3,7 +3,10 @@ package guru.springframework.apifirst.apifirstserver.services;
 import guru.springframework.apifirst.model.Customer;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CustomerService {
     List<Customer> listCustomers();
+
+    Customer getCustomerById(UUID customerId);
 }
