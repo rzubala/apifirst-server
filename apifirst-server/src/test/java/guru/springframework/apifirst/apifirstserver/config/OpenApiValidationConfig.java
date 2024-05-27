6f4f31829c1e7296038a3565bdf096410a8236d7
Rafał Zubala <rzubala@gmail.com>
2024-05-27 10:56:59 +0200

    new product

--- apifirst-server/src/test/java/guru/springframework/apifirst/apifirstserver/config/OpenApiValidationConfig.java
+++ apifirst-server/src/test/java/guru/springframework/apifirst/apifirstserver/config/OpenApiValidationConfig.java
@@ -9,23 +9,59 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Created by jt, Spring Framework Guru.
+ */
 @Configuration
 public class OpenApiValidationConfig {
+
     @Bean
     public Filter validationFilter() {
-        return new OpenApiValidationFilter(true, true);
+
+        return new OpenApiValidationFilter(
+                true, // enable request validation
+                true  // enable response validation
+        );
     }
+
     @Bean
-    public WebMvcConfigurer openApiValidationInterceptor() {
-        OpenApiInteractionValidator validator = OpenApiInteractionValidator.createForSpecificationUrl(
-                "https://api.redocly.com/registry/bundle/spring-framework-guru/API%20First%20With%20Spring%20Boot%20-%20Development/v1/openapi.yaml?branch=development"
-        ).build();
+    public WebMvcConfigurer openAPIValidationInterceptor() {
+
+        OpenApiInteractionValidator validator = OpenApiInteractionValidator.createForSpecificationUrl("https://api.redocly.com/registry/bundle/spring-framework-guru/API%20First%20With%20Spring%20Boot%20-%20Development/v1/openapi.yaml?branch=development")
+                .build();
+
         OpenApiValidationInterceptor interceptor = new OpenApiValidationInterceptor(validator);
+
         return new WebMvcConfigurer() {
+
             @Override
             public void addInterceptors(InterceptorRegistry registry) {
                 registry.addInterceptor(interceptor);
             }
         };
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- apifirst-server/src/test/java/guru/springframework/apifirst/apifirstserver/controllers/CustomerControllerTest.java
+++ apifirst-server/src/test/java/guru/springframework/apifirst/apifirstserver/controllers/CustomerControllerTest.java
@@ -1,34 +1,36 @@
 package guru.springframework.apifirst.apifirstserver.controllers;
 
-import lombok.val;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 
-
 import static org.hamcrest.Matchers.greaterThan;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+/**
+ * Created by jt, Spring Framework Guru.
+ */
 @SpringBootTest
-public class CustomerControllerTest extends BaseTest{
+public class CustomerControllerTest extends BaseTest {
 
-    @DisplayName("Test get customer by id")
+    @DisplayName("Get by Id")
     @Test
     void testGetCustomerById() throws Exception {
         mockMvc.perform(get(CustomerController.BASE_URL + "/{customerId}", testCustomer.getId())
-                .accept(MediaType.APPLICATION_JSON))
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(testCustomer.getId().toString()));
     }
 
-    @DisplayName("Test list customers")
+    @DisplayName("Test List Customers")
     @Test
     void testListCustomers() throws Exception {
         mockMvc.perform(get(CustomerController.BASE_URL)
-                .accept(MediaType.APPLICATION_JSON))
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.length()", greaterThan(0)));
     }
