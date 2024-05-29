41e046763e4f6a45242e6121b2e02230da7f0104
Rafał Zubala <rzubala@gmail.com>
2024-05-29 14:38:06 +0200

    order mapper

--- apifirst-server/src/main/java/guru/springframework/apifirst/apifirstserver/mappers/OrderMapper.java
+++ apifirst-server/src/main/java/guru/springframework/apifirst/apifirstserver/mappers/OrderMapper.java
@@ -5,13 +5,27 @@ import guru.springframework.apifirst.model.OrderCreateDto;
 import guru.springframework.apifirst.model.OrderDto;
 import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+/**
+ * Created by jt, Spring Framework Guru.
+ */
 @Mapper
 @DecoratedWith(OrderMapperDecorator.class)
 public interface OrderMapper {
-    Order dtoToOrder(OrderCreateDto dto);
+    @Mapping(target = "shipmentInfo", ignore = true)
+    @Mapping(target = "selectedPaymentMethod", ignore = true)
+    @Mapping(target = "orderStatus", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "dateUpdated", ignore = true)
+    @Mapping(target = "dateCreated", ignore = true)
+    @Mapping(target = "customer", ignore = true)
+    Order dtoToOrder(OrderCreateDto orderDto);
 
-    Order dtoToOrder(OrderDto dto);
+    @Mapping(target = "selectedPaymentMethod", ignore = true)
+    @Mapping(target = "dateUpdated", ignore = true)
+    @Mapping(target = "dateCreated", ignore = true)
+    Order dtoToOrder(OrderDto orderDto);
 
     OrderDto orderToDto(Order order);
 }
